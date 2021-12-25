a49d844665fbfa77efb53c9b1cc8a4b63d4627e6
Tom <tomhaaima@gmail.com>
2021-12-25 23:46:19 +0100

    added first bit of js

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,20 @@
+const numberButtons = document.querySelectorAll(["data-number"]);
+const operationButtons = document.querySelectorAll(["data-operation"]);
+const equalsButton = document.querySelector(["data-equals"]);
+const rootButton = document.querySelector(["data-root"]);
+const powerButton = document.querySelector(["data-power"]);
+const piButton = document.querySelector(["data-pi"]);
+const previousOperandTextElement = document.querySelector(
+	"[data-previous-operand]"
+);
+const currentOperandTextElement = document.querySelector(
+	"[data-current-operand]"
+);
 
+class Calculator {
+	constructor(previousOperandTextElement, currentOperandTextElement) {
+		this.previousOperandTextElement = previousOperandTextElement;
+		this.currentOperandTextElement = currentOperandTextElement;
+		this.clear();
+	}
+}
